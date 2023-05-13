aef7628c28c20a3999bf34b16b5cbd60b07167be
jassimshaji <66567742+jassimshaji@users.noreply.github.com>
2023-05-13 22:32:10 +0530

    final commit

--- datamappingphaseunwrapped.m
+++ datamappingphaseunwrapped.m
@@ -4,19 +4,25 @@
 % x = 901;
 % y = 565;
 % 
-x1 = 901;
-y1 = 572;
 % x1 = 901;
-% y1 = 585;
+% y1 = 572;
+%  x1 = 901;
+%  y1 = 585;
 % 
-% x2 = 1126;
-% y2 = 107;
+% x2 = 901;
+% y2 = 579;
+% x2 = 901;
+% y2 = 562;
 % 
-% x3 = 1126;
-% y3 = 107;
+% x3 = 901;
+% y3 = 554;
+% x3 = 901;
+% y3 = 535;
 % 
-% x4 = 1126;
-% y4 = 107;
+% x4 = 901;
+% y4 = 529;
+x4 = 901;
+y4 = 546;
 % 
 % % 0.5 ul
 % 
@@ -32,8 +38,8 @@ y1 = 572;
 % % 1 ul
 % 
 % 
-figure('Name','Phase difference_1ul_im_sol '); f = imshow("PhaseUnwrapped\1ul_withsol\Phasediff\Reconstructed_1ul_nopump_im_sol_uwphasediff.jpg");
-           datatip(f, x1,y1)
+% figure('Name','Phase difference_1ul_im_sol '); f = imshow("PhaseUnwrapped\1ul_withsol\Phasediff\Reconstructed_1ul_nopump_im_sol_uwphasediff.jpg");
+%            datatip(f, x1,y1)
 % figure('Name','Phase difference_1ul_min1_sol '); f = imshow("PhaseUnwrapped\1ul_withsol\Phasediff\Reconstructed_1ul_nopump_min1_sol_uwphasediff.jpg");
 %            datatip(f, x1,y1)
 % figure('Name','Phase difference_1ul_min5_sol '); f = imshow("PhaseUnwrapped\1ul_withsol\Phasediff\Reconstructed_1ul_nopump_min5_sol_uwphasediff.jpg");
@@ -70,8 +76,8 @@ figure('Name','Phase difference_1ul_im_sol '); f = imshow("PhaseUnwrapped\1ul_wi
 % 
 % % 100 ul
 % 
-% figure('Name','Phase difference_100ul_im_sol '); f = imshow("PhaseUnwrapped\100ul_withsol\Phasediff\Reconstructed_100ul_nopump_im_sol_uwphasediff.jpg");
-%            datatip(f, x4,y4)
+figure('Name','Phase difference_100ul_im_sol '); f = imshow("PhaseUnwrapped\100ul_withsol\Phasediff\Reconstructed_100ul_nopump_im_sol_uwphasediff.jpg");
+           datatip(f, x4,y4)
 % figure('Name','Phase difference_100ul_min1_sol '); f = imshow("PhaseUnwrapped\100ul_withsol\Phasediff\Reconstructed_100ul_nopump_min1_sol_uwphasediff.jpg");
 %            datatip(f, x4,y4)
 % figure('Name','Phase difference_100ul_min5_sol '); f = imshow("PhaseUnwrapped\100ul_withsol\Phasediff\Reconstructed_100ul_nopump_min5_sol_uwphasediff.jpg");
